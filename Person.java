798bd4511d464e0b471ca0b42a93f392ff7752a9
gzzzzy <61356650+gzzzzy@users.noreply.github.com>
2021-09-01 23:09:52 +0800

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,41 @@
+package DSC30.PA5;
+
+import java.util.*;
+
+public class Person {
+
+    private String name;
+    TreeSet<String> phoneNumberList;
+
+    public Person(String name, ArrayList<String> pnArray) {
+        this.name = name;
+        phoneNumberList = new TreeSet<>(pnArray);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean addPhoneNumber(String pn) {
+        if (phoneNumberList.contains(pn))
+            return false;
+        phoneNumberList.add(pn);
+        return true;
+    }
+
+    public ArrayList<String> getPhoneNumbers() {
+        return new ArrayList<>(phoneNumberList);
+    }
+
+    public boolean deletePhoneNumber(String pn) {
+        if (phoneNumberList.contains(pn)) {
+            if (phoneNumberList.size() == 1) {
+                throw new IllegalArgumentException();
+            } else {
+                return phoneNumberList.remove(pn);
+            }
+        } else {
+            return false;
+        }
+    }
+}
