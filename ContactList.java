c90af8fb5d676603880f3f0d0de7ab1e54ca11e0
gzzzzy <61356650+gzzzzy@users.noreply.github.com>
2021-09-01 22:30:53 +0800

    Add files via upload

---
+++ ContactList.java
@@ -0,0 +1,69 @@
+package DSC30.PA5;
+
+import java.util.*;
+
+public class ContactList {
+
+    private HashMap<String, Person> contactList=new HashMap<>();
+    TreeSet<String> nameList=new TreeSet<>();
+
+    public boolean createContact(Person person) {
+        if (contactList.containsKey(person.getName())) {
+            return false;
+        } else {
+            contactList.put(person.getName(), person);
+            nameList.add(person.getName());
+            return true;
+        }
+    }
+
+    public boolean lookupContact(String name) {
+        return contactList.containsKey(name);
+    }
+
+    public Person getContact(String name) {
+        if (contactList.containsKey(name)) {
+            return contactList.get(name);
+        } else {
+            return null;
+        }
+    }
+
+    public Person[] getContactByRange(String start, String end) {
+        if(start.compareTo(end)>=0)
+            throw new IllegalArgumentException();
+        ArrayList<Person> re=new ArrayList<>(size());
+        for (String name : nameList) {
+            if(name.compareTo(start)>=0&&name.compareTo(end)<0){
+                re.add(contactList.get(name));
+            }
+        }
+        return (Person[])re.toArray();
+    }
+
+    public boolean deleteContact(String name) {
+        if(lookupContact(name)){
+            contactList.remove(name);
+            nameList.remove(name);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int size() {
+        return contactList.size();
+    }
+
+    public String[] fetchAllNames() {
+        return (String[])nameList.toArray(new String[0]);
+    }
+
+    public String[] fetchAllPhoneNumbers() {
+        ArrayList<String> temp=new ArrayList<>();
+        for (String name : nameList) {
+            temp.addAll(contactList.get(name).getPhoneNumbers());
+        }
+        return (String[])temp.toArray(new String[0]);
+    }
+}
---
+++ Person.java
@@ -0,0 +1,39 @@
+package DSC30.PA5;
+
+import java.util.*;
+
+public class Person {
+
+    private String name;
+    TreeSet<String> phoneNumberList;
+
+    public Person(String name, ArrayList<String> pnArray) {
+        this.name = name;
+        phoneNumberList = new TreeSet<>(pnArray);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean addPhoneNumber(String pn) {
+        if (phoneNumberList.contains(pn))
+            return false;
+        phoneNumberList.add(pn);
+        return true;
+    }
+
+    public ArrayList<String> getPhoneNumbers() {
+        return new ArrayList<>(phoneNumberList);
+    }
+
+    public boolean deletePhoneNumber(String pn) {
+        if (pn == null || phoneNumberList.size() == 1)
+            throw new IllegalArgumentException();
+        if (phoneNumberList.remove(pn)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
